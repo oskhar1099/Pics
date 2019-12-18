1b24f68aee4a31d5665cba598cb248ca156b171e
Rails Course <oskhara@uninorte.edu.co>
2019-12-17 22:56:37 -0500

    Added async function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () =>{
-    return(
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+    async onSearchSubmit(term){
+        const response = await axios
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {query: term},
+            headers:{
+                Authorization: 'Client-ID 1247ee7dcbe4d05d44e3a0497af29f7f28f666b700df69831bcceb7ac55ac729'                
+            }       
+        });
+
+        console.log(response.data.results);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
 };
 
 export default App;
